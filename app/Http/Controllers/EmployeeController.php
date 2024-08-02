3c83e2e38ab8ff040a78fee66b02ad8250afc85b
Yannick <yannick.kupferschmidt@googlemail.com>
2024-08-02 13:59:41 +0200

    feat: added basic route structure and EmployeeController

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Import employees from a CSV file
+     */
+    public function import(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/employee', [EmployeeController::class, 'import']);
+Route::get('/employee', [EmployeeController::class, 'index']);
+Route::get('/employee/{id}', [EmployeeController::class, 'show']);
+Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);
